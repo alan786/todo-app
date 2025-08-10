fd6c75bb10b14992a01e8beab0db2ec164d6a3e0
alan786 <alanlin786@gmail.com>
2025-08-10 17:51:08 -0500

    adding edit

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,17 @@ import { Header } from './component/Header'
 import { TodoList } from './component/TodoList'
 import { TodoInput } from './component/TodoInput'
 
-
 function App() {
-  // const todos = [
-  // { input: 'Hello! Add your first todo!', complete: true },
-  // { input: 'Get the groceries!', complete: false },
-  // { input: 'Learn how to web design', complete: false },
-  // { input: 'Say hi to gran gran', complete: true },
-  // ]
-
   const [todos, setTodos] = useState([
     { input: 'Hello! Add your first todo!', complete: true }
   ]);
-
   const [selectedTab, setSelectedTab] = useState('Open');
- 
 
   function handleAddTodo(newTodo) {
     const newTodoList = [...todos, {input: newTodo, complete: false}]
     setTodos(newTodoList)
     handleSaveData(newTodoList)
   }
-
 
   function handleCompleteTodo(index) {
     let newTodoList = [...todos]
@@ -39,42 +28,47 @@ function App() {
     handleSaveData(newTodoList)
   }
 
-  function handleDeleteTodo(Index) {
+  // NEW: Handle todo updates
+  function handleUpdateTodo(index, updatedText) {
+    const newTodoList = [...todos];
+    newTodoList[index] = {
+      ...newTodoList[index],
+      input: updatedText
+    };
+    setTodos(newTodoList);
+    handleSaveData(newTodoList);
+  }
+
+  function handleDeleteTodo(index) {
     const newTodoList = todos.filter((val, valIndex) => {
-      return valIndex !== Index
+      return valIndex !== index
     })
     setTodos(newTodoList)
     handleSaveData(newTodoList)
-    
   }
 
   function handleSaveData(currentTodos) {
     localStorage.setItem('todo-app', JSON.stringify({ todos: currentTodos}))
   }
 
-
   useEffect(() => {
     if (!localStorage || !localStorage.getItem('todo-app')) {return}
     let db = JSON.parse(localStorage.getItem('todo-app'))
-     setTodos(db.todos)
-
-    }
-    
-  , []
-  )
-
-  
-
-
+    setTodos(db.todos)
+  }, [])
 
   return (
     <>
-
       <Header todos={todos} />
       <Tabs todos={todos} selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      <TodoList handleDeleteTodo={handleDeleteTodo} handleCompleteTodo={handleCompleteTodo} selectedTab={selectedTab} todos={todos} />
+      <TodoList 
+        handleDeleteTodo={handleDeleteTodo} 
+        handleCompleteTodo={handleCompleteTodo} 
+        handleUpdateTodo={handleUpdateTodo}  // Pass update function
+        selectedTab={selectedTab} 
+        todos={todos} 
+      />
       <TodoInput handleAddTodo={handleAddTodo} />
-  
     </>
   )
 }
--- src/component/TodoCard.jsx
+++ src/component/TodoCard.jsx
@@ -1,23 +1,53 @@
+import { useState } from 'react';
+
 export function TodoCard(props) {
-    const {todo, handleDeleteTodo, todoIndex, handleCompleteTodo} = props
-   
+    const {todo, handleDeleteTodo, todoIndex, handleCompleteTodo, handleUpdateTodo} = props;
+    const [isEditing, setIsEditing] = useState(false);
+    const [editText, setEditText] = useState(todo.input);
+
+    const handleSaveEdit = () => {
+        handleUpdateTodo(todoIndex, editText);
+        setIsEditing(false);
+    };
 
     return (
         <div className="card todo-item">
-            <p>{todo.input}</p>
-            <div className="todo-buttons">
-                <button onClick={() => {
-                    handleCompleteTodo(todoIndex)
-                }} disabled={todo.complete}>
-                    <h6>Done</h6>
-                </button>
-                <button onClick={() => {
-                    handleDeleteTodo(todoIndex)
-                }}>
-                    <h6>Delete</h6>
-                </button>
-            </div>
-
+            {isEditing ? (
+                <div className="edit-mode">
+                    <input
+                        type="text"
+                        value={editText}
+                        onChange={(e) => setEditText(e.target.value)}
+                        autoFocus
+                    />
+                    <div className="edit-buttons">
+                        <button onClick={handleSaveEdit}>
+                            <h6>Save</h6>
+                        </button>
+                        <button onClick={() => setIsEditing(false)}>
+                            <h6>Cancel</h6>
+                        </button>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <p>{todo.input}</p>
+                    <div className="todo-buttons">
+                        <button 
+                            onClick={() => handleCompleteTodo(todoIndex)} 
+                            disabled={todo.complete}
+                        >
+                            <h6>Done</h6>
+                        </button>
+                        <button onClick={() => setIsEditing(true)}>
+                            <h6>Edit</h6>
+                        </button>
+                        <button onClick={() => handleDeleteTodo(todoIndex)}>
+                            <h6>Delete</h6>
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
